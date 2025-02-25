18317561af5ff30768093a93d1960e0ea867f257
KL3FT3Z <toxy4ny@parasite.tech>
2025-02-25 13:22:25 +0200

    Add files via upload

---
+++ bountyfruits-kali.sh
@@ -0,0 +1,155 @@
+#!/bin/bash
+
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[1;34m'
+MAGENTA='\033[1;35m'
+CYAN='\033[1;36m'
+NC='\033[0m'
+
+echo -e "${CYAN}░▒▓███████▓▒░  ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓████████▓▒░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓████████▓▒░▒▓███████▓▒░"
+echo -e "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  ${YELLOW}░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░  ░▒▓█▓▒░  ░▒▓█▓▒░"
+echo -e "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░  ░▒▓█▓▒░  ░▒▓█▓▒░"
+echo -e "${RED}░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░    ░▒▓██████▓▒░       ░▒▓██████▓▒░ ░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓██████▓▒░"
+echo -e "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░${MAGENTA}      ░▒▓█▓▒░          ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░  ░▒▓█▓▒░         ░▒▓█▓▒░"
+echo -e "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░${BLUE}      ░▒▓█▓▒░          ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░  ░▒▓█▓▒░         ░▒▓█▓▒░"
+echo -e "░▒▓███████▓▒░${GREEN}  ░▒▓██████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░      ░▒▓█▓▒░          ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓█▓▒░   ░▒▓█▓▒░░░▒▓███████▓▒░${NC}"
+echo -e "                                                                                                                        "
+echo -e "                                                                                                                        ${CYAN}KL3FT3Z https://github.com/toxy4ny${NC}"
+echo ""
+echo -e "${YELLOW}The following fruits will be installed::${NC}"
+echo -e "- dnscan"
+echo -e "- Knockpy"
+echo -e "- Sublist3r"
+echo -e "- massdns"
+echo -e "- nmap"
+echo -e "- masscan"
+echo -e "- EyeWitness"
+echo -e "- DirBuster"
+echo -e "- dirsearch"
+echo -e "- Gitrob"
+echo -e "- git-secrets"
+echo -e "- sandcastle"
+echo -e "- GoogD0rker"
+echo -e "- waybackurls"
+echo -e "- Sn1per"
+echo -e "- XRay"
+echo -e "- wfuzz"
+echo -e "- patator"
+echo -e "- datasploit"
+echo -e "- hydra"
+echo -e "- changeme"
+echo -e "- MobSF"
+echo -e "- Apktool"
+echo -e "- dex2jar"
+echo -e "- sqlmap"
+echo -e "- oxmlxxe"
+echo -e "- XXE Injector"
+echo -e "- jwt_tool"
+echo -e "- ground-control"
+echo -e "- ssrfDetector"
+echo -e "- LFISuit"
+echo -e "- GitTools"
+echo -e "- dvcs-ripper"
+echo -e "- tko-subs"
+echo -e "- HostileSubBruteforcer"
+echo -e "- Race the Web"
+echo -e "- ysoserial"
+echo -e "- PHPGGC"
+echo -e "- CORStest"
+echo -e "- retire.js"
+echo -e "- getsploit"
+echo -e "- Findsploit"
+echo -e "- bfac"
+echo -e "- WPScan"
+echo -e "- CMSMap"
+echo -e "- Amass"
+
+echo ""
+read -p "Do you agree to install all of these Fruits? (y/n): " -n 1 -r
+echo ""
+
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    echo "Installation abort."
+    exit 1
+fi
+
+echo "Let's go..."
+
+install_package() {
+    local package_name=$1
+    if ! dpkg -l | grep -qw $package_name; then
+        echo "Installation $package_name..."
+        sudo apt-get update
+        sudo apt-get install -y $package_name
+    else
+        echo "$package_name Installed."
+    fi
+}
+
+install_package "git"
+install_package "python3"
+install_package "python3-pip"
+
+install_tool() {
+    local tool_name=$1
+    local install_command=$2
+    local check_command=$3
+
+    if ! command -v $check_command &> /dev/null; then
+        echo "Installation $tool_name..."
+        eval $install_command
+    else
+        echo "$tool_name Installed."
+    fi
+}
+
+install_tool "dnscan" "sudo apt-get install -y dnscan" "dnscan"
+install_tool "Knockpy" "git clone https://github.com/guelfoweb/knock.git && cd knock && sudo python3 setup.py install" "knockpy"
+install_tool "Sublist3r" "git clone https://github.com/aboul3la/Sublist3r.git && cd Sublist3r && pip3 install -r requirements.txt" "sublist3r"
+install_tool "massdns" "git clone https://github.com/blechschmidt/massdns.git && cd massdns && make" "massdns"
+install_tool "nmap" "sudo apt-get install -y nmap" "nmap"
+install_tool "masscan" "sudo apt-get install -y masscan" "masscan"
+install_tool "EyeWitness" "git clone https://github.com/ChrisTruncer/EyeWitness.git && cd EyeWitness/setup && ./setup.sh" "eyewitness"
+install_tool "DirBuster" "sudo apt-get install -y dirbuster" "dirbuster"
+install_tool "dirsearch" "git clone https://github.com/maurosoria/dirsearch.git" "dirsearch.py"
+install_tool "Gitrob" "sudo gem install gitrob" "gitrob"
+install_tool "git-secrets" "git clone https://github.com/awslabs/git-secrets.git && cd git-secrets && sudo make install" "git-secrets"
+install_tool "sandcastle" "git clone https://github.com/yasinS/sandcastle.git" "sandcastle"
+install_tool "GoogD0rker" "git clone https://github.com/ZephrFish/GoogD0rker.git" "googD0rker.py"
+install_tool "waybackurls" "go install github.com/tomnomnom/waybackurls@latest" "waybackurls"
+install_tool "Sn1per" "git clone https://github.com/1N3/Sn1per.git && cd Sn1per && bash install.sh" "sniper"
+install_tool "XRay" "git clone https://github.com/evilsocket/xray.git && cd xray && make" "xray"
+install_tool "wfuzz" "sudo apt-get install -y wfuzz" "wfuzz"
+install_tool "patator" "git clone https://github.com/lanjelot/patator.git" "patator.py"
+install_tool "datasploit" "git clone https://github.com/DataSploit/datasploit.git && cd datasploit && pip3 install -r requirements.txt" "datasploit"
+install_tool "hydra" "sudo apt-get install -y hydra" "hydra"
+install_tool "changeme" "git clone https://github.com/ztgrace/changeme.git" "changeme"
+install_tool "MobSF" "git clone https://github.com/MobSF/Mobile-Security-Framework-MobSF.git && cd Mobile-Security-Framework-MobSF && sudo ./setup.sh" "run.sh"
+install_tool "Apktool" "sudo apt-get install -y apktool" "apktool"
+install_tool "dex2jar" "sudo apt-get install -y dex2jar" "d2j-dex2jar"
+install_tool "sqlmap" "sudo apt-get install -y sqlmap" "sqlmap"
+install_tool "oxmlxxe" "git clone https://github.com/BuffaloWill/oxml\xxe.git" "oxmlxxe"
+install_tool "XXE Injector" "git clone https://github.com/enjoiz/XXEinjector.git" "XXEinjector.pl"
+install_tool "jwt_tool" "git clone https://github.com/ticarpi/jwt_tool.git && cd jwt_tool && pip3 install -r requirements.txt" "jwt_tool.py"
+install_tool "ground-control" "git clone https://github.com/jobertabma/ground-control.git" "ground-control"
+install_tool "ssrfDetector" "git clone https://github.com/JacobReynolds/ssrfDetector.git" "ssrfDetector"
+install_tool "LFISuit" "git clone https://github.com/D35m0nd142/LFISuite.git" "LFISuite.py"
+install_tool "GitTools" "git clone https://github.com/internetwache/GitTools.git" "gitdumper"
+install_tool "dvcs-ripper" "git clone https://github.com/kost/dvcs-ripper.git" "dvcs-ripper"
+install_tool "tko-subs" "git clone https://github.com/anshumanbh/tko-subs.git && cd tko-subs && go build" "tko-subs"
+install_tool "HostileSubBruteforcer" "git clone https://github.com/nahamsec/HostileSubBruteforcer.git" "hostile_sub_bruteforcer"
+install_tool "Race the Web" "git clone https://github.com/insp3ctre/race-the-web.git" "race-the-web"
+install_tool "ysoserial" "git clone https://github.com/GoSecure/ysoserial.git && cd ysoserial && ./gradlew build" "ysoserial"
+install_tool "PHPGGC" "git clone https://github.com/ambionics/phpggc.git" "phpggc"
+install_tool "CORStest" "git clone https://github.com/RUB-NDS/CORStest.git" "CORStest"
+install_tool "retire.js" "sudo apt-get install -y nodejs npm && sudo npm install -g retire" "retire"
+install_tool "getsploit" "git clone https://github.com/vulnersCom/getsploit.git && cd getsploit && pip3 install -r requirements.txt" "getsploit"
+install_tool "Findsploit" "git clone https://github.com/1N3/Findsploit.git && cd Findsploit && bash install.sh" "findsploit"
+install_tool "bfac" "git clone https://github.com/mazen160/bfac.git" "bfac"
+install_tool "WPScan" "sudo apt-get install -y wpscan" "wpscan"
+install_tool "CMSMap" "git clone https://github.com/Dionach/CMSmap.git && cd CMSmap && pip3 install -r requirements.txt" "cmsmap"
+install_tool "Amass" "sudo apt-get install -y amass" "amass"
+
+echo "Full installation complete!"
